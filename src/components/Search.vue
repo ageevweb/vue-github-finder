9a46fb0f0fb3a08c7e8142975e56940dab0d67fb
ageevweb <ageevweb.com>
2020-04-21 21:58:44 +0300

    get user repo API

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -31,7 +31,6 @@ export default {
   
   watch: {
     search (val) {
-      console.log(val)
       this.$emit('search', val);
     }
   }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,20 @@
   .wrapper-content.wrapper-content--fixed
     section
       .container
-        search(:value="search" placeholder="type github's name...")
+        search(
+          :value="search" 
+          placeholder="type github's name..."
+          @search="search = $event"
+        )
+        button.btn.btnPrimary(@click="getRepo") search
+
 
 </template>
 
 
 <script>
 import search from '@/components/Search.vue'
+import axios from 'axios'
 
 export default {
 
@@ -19,6 +26,18 @@ export default {
       search: '',
     }
   },
+  methods: {
+    getRepo(){
+      axios
+        .get(`https://api.github.com/users/${this.search}/repos`)
+          .then(res => {
+            console.log(res.data)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    }
+  }
 
 }
 </script>
